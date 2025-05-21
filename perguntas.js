a3b014cbd8b837aafa721dcb32c3408088971200
bibikarine <160857488+bibikarine@users.noreply.github.com>
2025-05-21 18:03:19 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'CONHECIMENTOS GERAIS',
-    'Qual é a principal atividade econômica do campo?',
-    'Agricultura e pecuária.'
+    'PERGUNTAS',
+    'Qual é o principal impacto ambiental causado pela expansão das cidades sobre o campo?',
+    'Desmatamento e perda de áreas agrícolas.'
 )
 
 criaCartao(
-    'QUÍMICA',
-    'Qual é a principal atividade econômica do campo?',
-    'Agricultura e pecuária.'
+    'PERGUNTAS',
+    ' Por que muitas pessoas se mudam do campo para a cidade?',
+    'Em busca de trabalho, estudo e melhores condições de vida.'
 )
 
 criaCartao(
-    'PORTUGUÊS',
-    'Qual é o plural de "coração"?',
-    'Corações.'
+    'PERGUNTAS',
+    'Como se chama a área de transição entre o campo e a cidade?',
+    'Periferia ou zona periurbana.'
 )
 
 criaCartao(
-    'ARTE',
-    'Quem pintou a obra "A Última Ceia?',
-    'Leonardo da Vinci.'
+    'CURIOSIDADES',
+    'Você sabia que muitos alimentos típicos das festas urbanas vêm diretamente do campo?',
+    'Produtos como milho, amendoim e mandioca são colhidos no interior e viram delícias nas festas das cidades!'
 )
 
 criaCartao(
-    'BIOLOGIA',
-    'Qual a função dos ribossomos  na célula?',
-    'Produzir proteínas.'
+    'CURIOSIDADES',
+    'O campo e a cidade se unem também na economia durante as festas?',
+    'Com certeza! Agricultores vendem mais produtos, e comerciantes da cidade aproveitam para impulsionar as vendas com itens típicos!'
 )
 
 criaCartao(
-    'MATEMÁTICA',
-    ' Qual é a fórmula da área de um triângulo?',
-    'Área = (base x altura)/2'
+    'CURIOSIDADES',
+    'Voce sabia que as festas juninas nas cidades são inspiradas em tradições do campo?',
+    'AQuadrilhas, roupas caipiras e até as danças vieram da vida rural e foram adaptadas para o ambiente urbano.'
 )
